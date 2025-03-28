acbc81c430033e0747d3bff541fa55ac1d6c651a
gennievtanke337 <novoeprostrabstvo@gmail.com>
2025-03-28 20:28:52 +0200

    s

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const accountTab = document.getElementById("accountTab");
+    const homeTab = document.getElementById("homeTab");
     const logoutBtn = document.getElementById("logoutBtn");
     const homeSection = document.getElementById("homeSection");
     const accountSection = document.getElementById("accountSection");
@@ -8,20 +9,23 @@ document.addEventListener("DOMContentLoaded", function () {
     const closeModalBtn = document.getElementById("closeModalBtn");
     const savePasswordBtn = document.getElementById("savePasswordBtn");
     const usernameDisplay = document.getElementById("usernameDisplay");
+    const accountUsername = document.getElementById("accountUsername");
 
-    let username = localStorage.getItem("username");
-
-    if (!username) {
-        username = prompt("Введіть ваше ім'я яке буде показуватись користувачам") || "Користувач";
-        localStorage.setItem("username", username);
-    }
+    let username = localStorage.getItem("username") || "Користувач";
 
     usernameDisplay.textContent = username;
+    accountUsername.textContent = username;
 
     accountTab.addEventListener("click", function (event) {
         event.preventDefault();
         homeSection.classList.add("hidden");
         accountSection.classList.remove("hidden");
+    });
+
+    homeTab.addEventListener("click", function (event) {
+        event.preventDefault();
+        accountSection.classList.add("hidden");
+        homeSection.classList.remove("hidden");
     });
 
     logoutBtn.addEventListener("click", function () {
@@ -36,10 +40,10 @@ document.addEventListener("DOMContentLoaded", function () {
     savePasswordBtn.addEventListener("click", function () {
         const newPassword = document.getElementById("newPassword").value;
         if (newPassword.trim() !== "") {
-            alert("Пароль змінено успішно");
+            alert("Пароль змінено успішно!");
             passwordModal.classList.add("hidden");
         } else {
-            alert("Введіть новий пароль");
+            alert("Введіть новий пароль!");
         }
     });
 
